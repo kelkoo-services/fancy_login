cc6408795da370363e674f22b730626b487dd28f
Jose Toscano <josetoscanogil@gmail.com>
2017-10-17 12:57:03 +0200

    updated gem version

--- lib/fancy_login/version.rb
+++ lib/fancy_login/version.rb
@@ -1,3 +1,3 @@
 module FancyLogin
-  VERSION = "1.0.53" # Removed facebook url from button to avoid 404 bots errors
+  VERSION = "1.0.54" # Removed facebook url from button to avoid 404 bots errors
 end
